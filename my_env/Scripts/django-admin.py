3e1b2f17b10f0a6a34d70cccf88fb1d074267bfb
krispin <krispinblow@gmail.com>
2020-10-20 20:31:20 -0500

    my initial commit

---
+++ my_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\krispin\documents\documents\masters\mscisba\cidm\3_fall_2020\cidm_6325\assignments\assignment_1\cidm6325-master\my_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
